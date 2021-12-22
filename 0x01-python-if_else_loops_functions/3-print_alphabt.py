f40e54bce3a18bea3510178ff60d615c89251ad6
ammartica <amisadaymartinez@gmail.com>
2021-12-22 02:49:03 +0000

    changed loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if chr(c) != 'e' and chr(c) != 'q':
-        print(chr(c), end = '')
+for a in range(ord('a'), ord('z') + 1):
+    if chr(a) != 'e' and chr(a) != 'q':
+        print("{:c}".format(a), end='')
 
