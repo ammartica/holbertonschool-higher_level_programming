fbb5cdce158bdd58f1e27c3d3b021357647ec2dc
ammartica <amisadaymartinez@gmail.com>
2021-12-29 19:32:35 +0000

    shortened line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-        "." if l == 1 else ":"))
-    # enumerate() returns count of current iteration (i) and value of argument (s for string)
+    lar = len(argv)
+    print("{:d} {:s}{:s}".format(lar - 1, "argument" if lar <= 2 else "arguments", "." if lar == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
