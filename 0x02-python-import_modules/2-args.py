727b8dc714884ccfc77fcd344c923bb5ecb36e38
ammartica <amisadaymartinez@gmail.com>
2021-12-29 19:50:39 +0000

    fixed logic error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    lar = len(argv)
-    print("{:d} {:s}{:s}".format(lar - 1, "argument" if lar <= 2 else "arguments", "." if lar == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(count, "arguments:")
+    for i in range(count):
+        print("{}: {}".format(i + 1, argv[i + 1]))
