0aa50ac82dfea1d9f74045d88e1c9cd3150ab8aa
ammartica <amisadaymartinez@gmail.com>
2022-01-04 01:19:01 +0000

    Added conditional

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     cpy = my_list.copy()
     if idx < 0:
         return cpy
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return cpy
     else:
         cpy[idx] = element
