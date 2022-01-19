55b8903ae9fc33ab72aab6e2bbc5e779973cc8f7
ammartica <amisadaymartinez@gmail.com>
2022-01-19 23:51:01 +0000

    ADD file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class named Square"""
+
+
+class Square:
+    """Defines a square"""
+
+
+    def __init__(self, size):
+        """Method that initializes square
+
+        Args:
+            size (int): size of a square
+
+        Returns:
+            Nothing
+
+        """
+        self.__size = size
