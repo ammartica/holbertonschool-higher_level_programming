fc16b779d60d0642c56e726e3f66c0251be8717d
ammartica <amisadaymartinez@gmail.com>
2022-02-01 13:32:05 +0000

    ADD file

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module named 3-say_my_name
+
+    Contains function named say_my_name(first_name, last_name="")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints My name is <first name> <last name>
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
