ff01586f2af5a023c8013922c2b74eaf03701b5c
ammartica <amisadaymartinez@gmail.com>
2022-02-03 15:44:10 +0000

    ADD file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module contains inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """returns bool if object is instance of a class that inherited from specified class"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
