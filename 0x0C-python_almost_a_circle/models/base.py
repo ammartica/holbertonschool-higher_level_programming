7ecf18f6f66b828d5802e17417f06c2c27dcd4f0
ammartica <amisadaymartinez@gmail.com>
2022-02-04 17:46:09 +0000

    ADD file

---
+++ 0x0C-python_almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains class named Base"""
+
+
+class Base:
+    """Class named Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize Base class"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
