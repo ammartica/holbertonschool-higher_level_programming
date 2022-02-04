bd5840445b2a11a22618327023f7992064fceb6a
ammartica <amisadaymartinez@gmail.com>
2022-02-04 18:48:12 +0000

    task 3

--- 0x0C-python_almost_a_circle/models/rectangle.py
+++ 0x0C-python_almost_a_circle/models/rectangle.py
@@ -10,10 +10,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """initializes rectangle"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -38,15 +38,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter for private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @height.setter
     def height(self, value):
         """setter for private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @x.setter
     def x(self, value):
         """setter for private instance attribute x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @y.setter
     def y(self, value):
         """setter for private instance attribute y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
