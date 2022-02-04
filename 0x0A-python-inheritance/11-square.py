8bffdfa73ac602f68b19934d9537689fcfdb40f9
ammartica <amisadaymartinez@gmail.com>
2022-02-04 16:56:15 +0000

    ADD file

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Subclass named Square inherits from BaseGeometry and Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represents a square"""
+    def __init__(self, size):
+        """instantiation of Square class"""
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """returns area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """print and return square description"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
