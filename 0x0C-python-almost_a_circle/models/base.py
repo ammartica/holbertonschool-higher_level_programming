d4f1b3b0ef2e6ce42a5b0b3d4e24cc301c203c83
ammartica <amisadaymartinez@gmail.com>
2022-02-07 17:08:44 +0000

    ADD file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Contains a class named Base"""
+
+import json
+
+
+class Base:
+    """Class named Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize Base class"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON string representation of list_dictionaries"""
+        if not list_dictionaries or list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string of list_objs to a file"""
+        if list_objs is None:
+            ls = []
+        file_name = cls.__name__ + ".jason"
+        with open(file_name, "w") as f:
+            f.write(cls.to_json_string(list_objs))
+
