088f33927b5bf8c8942bbba8e7b468bd00902c4d
ammartica <amisadaymartinez@gmail.com>
2022-02-07 00:18:31 +0000

    ADD file

---
+++ 0x0C-python_almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""child class Square of Rectangle"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """subclass Square inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes Square"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """returns [Square] (<id>) <x>/<y> - <size>"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.size)
