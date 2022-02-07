8573e305e94c4d97bf92187132e338ab0457986c
ammartica <amisadaymartinez@gmail.com>
2022-02-07 00:52:49 +0000

    task 13

--- 0x0C-python_almost_a_circle/models/rectangle.py
+++ 0x0C-python_almost_a_circle/models/rectangle.py
@@ -113,3 +113,8 @@ class Rectangle(Base):
             if kwargs is not None:
                 for key, value in kwargs.items():
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns dict representation of a Rectangle"""
+        return {'id':self.id, 'width':self.width, 'height':self.height,
+                'x':self.x, 'y':self.y}
