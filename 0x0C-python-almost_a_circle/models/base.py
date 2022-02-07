d4c6d6eb2905920327da733df1bb0426fb4ff49e
ammartica <amisadaymartinez@gmail.com>
2022-02-07 17:23:08 +0000

    task 15: modified conditional statement

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string representation of list_dictionaries"""
-        if not list_dictionaries or list_dictionaries is None:
+        if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
 
