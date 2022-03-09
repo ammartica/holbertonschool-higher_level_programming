3a0905c766b81704bda03656d85c819fc4ac3e00
ammartica <amisadaymartinez@gmail.com>
2022-03-09 01:52:07 +0000

    added ASC order

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 --script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 
-SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
