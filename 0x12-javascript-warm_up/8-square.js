1a8102a283456be70856d505a5f46a7fb7cb32a5
ammartica <amisadaymartinez@gmail.com>
2022-04-04 16:57:29 +0000

    fixed indentations in line 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,8 @@ const x = process.argv[2];
 
 if (parseInt(x)) {
   for (let i = 0; i < x; i++) {
-      console.log('X'.repeat(x)); }
+    console.log('X'.repeat(x));
+  }
 } else {
   console.log('Missing size');
 }
