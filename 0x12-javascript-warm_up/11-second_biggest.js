09e095050b4a63d31b9509e8249cf4f684de8083
ammartica <amisadaymartinez@gmail.com>
2022-04-04 21:57:07 +0000

    changed console.log result from string to number ('0' to 0)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const len = process.argv.length;
 const argList = process.argv.slice(2);
 
 if (len <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   argList.sort().reverse();
   const secondBig = argList[1];
