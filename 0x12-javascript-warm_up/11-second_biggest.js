2a4cd0da5c4ec6e042fa6241de54140eb1ee1347
ammartica <amisadaymartinez@gmail.com>
2022-04-04 21:55:02 +0000

    fixed logic mistake: if len == 1 it should print 0

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,10 @@
 const len = process.argv.length;
 const argList = process.argv.slice(2);
 
-if (len < 3) {
+if (len <= 3) {
   console.log('0');
-} else if (len === 3) {
-  console.log('1');
 } else {
   argList.sort().reverse();
-  const secondLargest = argList[1];
-  console.log(secondLargest);
+  const secondBig = argList[1];
+  console.log(secondBig);
 }
