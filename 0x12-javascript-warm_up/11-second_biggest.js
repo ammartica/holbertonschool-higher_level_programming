6af64561f0a58f13d689ef6854621d395f42378c
ammartica <amisadaymartinez@gmail.com>
2022-04-04 22:07:48 +0000

    exchanged reverse method for sort method in descending order

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const len = process.argv.length;
 const argList = process.argv.slice(2);
 
 if (len > 3) {
-  argList.sort().reverse();
+  argList.sort(function (a, b) { return b - a; });
   const secondBig = argList[1];
   console.log(secondBig);
 } else {
