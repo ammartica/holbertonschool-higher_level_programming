9e96939bda6536c32f239eb3898f93e74a86d3e2
ammartica <amisadaymartinez@gmail.com>
2022-04-04 16:56:30 +0000

    ADD file

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints a square the size of x
+
+const x = process.argv[2];
+
+if (parseInt(x)) {
+  for (let i = 0; i < x; i++) {
+      console.log('X'.repeat(x)); }
+} else {
+  console.log('Missing size');
+}
