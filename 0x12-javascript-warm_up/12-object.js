294dd2361d27bc314620aa5a149327a87feba435
ammartica <amisadaymartinez@gmail.com>
2022-04-04 22:14:41 +0000

    ADD file

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Update script to replace value 12 with value 89
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject);
