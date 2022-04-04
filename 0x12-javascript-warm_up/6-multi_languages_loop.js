40ada1d792b238110f79404c74d9f8e7a8e61746
ammartica <amisadaymartinez@gmail.com>
2022-04-04 16:36:34 +0000

    ADD file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Prints 3 lines using an array of string and a loop
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
