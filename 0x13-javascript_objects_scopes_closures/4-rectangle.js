90c1a0e6e18a5a957387fa2357536a58dd2e6b0d
ammartica <amisadaymartinez@gmail.com>
2022-04-05 20:22:11 +0000

    ADD file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+// Class Rectangle creates rectangle with instance methods rotate() and double()
+
+class Rectangle {
+  constructor (w, h) {
+    if (w && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Instance method print() prints a rectangle using character X
+  print () {
+    let stringRect = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        stringRect += 'X';
+      }
+      if (i !== this.height - 1) {
+        stringRect += '\n';
+      }
+    }
+    console.log(stringRect);
+  }
+
+  // Instance method rotate() exchanges width and height
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  // Instance method double() multiplies the width and the height by 2
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
