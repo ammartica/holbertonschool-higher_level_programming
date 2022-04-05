9fd46b13f7f2404aebbc5345feb68f4d8f9235f5
ammartica <amisadaymartinez@gmail.com>
2022-04-05 20:56:08 +0000

    ADD file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Child Class Square with instance method charPrint(c)
+
+const fSquare = require('./5-square.js');
+
+class Square extends fSquare {
+  // Instance method prints rectangle using the character c
+  charPrint (c) {
+    super.print(c);
+  }
+}
+
+module.exports = Square;
