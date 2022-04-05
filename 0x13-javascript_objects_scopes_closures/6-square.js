991a443166a09f39514dc3fd6632eb1b3ce8bd33
ammartica <amisadaymartinez@gmail.com>
2022-04-05 21:08:28 +0000

    changed code back to ADD file code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,11 @@
 
 const fSquare = require('./5-square.js');
 
-module.exports = class Square extends fSquare {
+class Square extends fSquare {
   // Instance method prints rectangle using the character c
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
     super.print(c);
   }
-};
+}
+
+module.exports = Square;
