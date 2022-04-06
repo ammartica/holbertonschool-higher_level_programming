176435244322284f4ffdaae2cc1cbb2f45f26e95
ammartica <90794598+ammartica@users.noreply.github.com>
2022-04-05 20:16:03 -0400

    Delete 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// Concats 2 files
-
-const fs = require('fs');
-let info = '';
-
-info = info.concat(fs.readFileSync(process.argv[2]));
-info = info.concat(fs.readFileSync(process.argv[3]));
-fs.writeFileSync(process.argv[4], info);
