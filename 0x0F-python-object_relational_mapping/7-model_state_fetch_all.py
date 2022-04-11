cfcc11a66152e1c5ef297ac40a7da7cc966304b6
ammartica <amisadaymartinez@gmail.com>
2022-04-11 20:35:31 +0000

    ADD file

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists all State objects from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    """create engine to connect to database using log in sent as arguments"""
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+             argv[1], argv[2], argv[3]))
+
+    """create a session to talk to the database"""
+    session = sessionmaker(bind=engine)()
+
+    """store desired State objects in results var"""
+    results = session.query(State).order_by(State.id)
+
+    """display State objects"""
+    for state in results:
+        print("{}: {}".format(state.id, state.name))
+
+    """close session"""
+    session.close()
