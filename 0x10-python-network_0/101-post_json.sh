cdf68af05495ddfdb06be1632df9b1addaa55cf0
ammartica <amisadaymartinez@gmail.com>
2022-04-22 18:47:49 +0000

    fixed flag typo

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sHX POST "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
