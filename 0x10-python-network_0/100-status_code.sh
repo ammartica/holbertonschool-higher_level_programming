24c177b599eb72d81895ff11ee1f01c9252c39d2
ammartica <amisadaymartinez@gmail.com>
2022-04-22 18:07:59 +0000

    added comment line

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,1 +1,3 @@
-curl -s -o /dev/null -w "%{http_code}" "$1"
+#!/bin/bash
+#sends a request to a URL passed as an argument, and displays only the status code of the response
+curl -so /dev/null -w "%{http_code}" "$1"
