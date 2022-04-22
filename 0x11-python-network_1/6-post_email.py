6f726c32ce6da6bcad49d6b034b0fcbf103ee7a0
ammartica <amisadaymartinez@gmail.com>
2022-04-22 15:29:31 +0000

    ADD file

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally
+displays the body of the response.
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    req = requests.post(url, values)
+    print(req.text)
