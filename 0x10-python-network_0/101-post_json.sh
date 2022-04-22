4f4966440bb79cc886d5d18cf47c74cd3c27f80a
ammartica <amisadaymartinez@gmail.com>
2022-04-22 18:46:18 +0000

    Added X flag

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sH POST "Content-Type: application/json" -d @"$2" "$1"
+curl -sHX POST "Content-Type: application/json" -d @"$2" "$1"
